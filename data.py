c95aa262c460c3f5ebccb8d1abb4294d67cff108
murali <mk60607@gmail.com>
2022-09-28 08:11:24 +0000

    final code

--- data.py
+++ data.py
@@ -1,8 +1,7 @@
-data=[2,3,'reddy',891,'murali',2,'MURALI']
-data.append("23")
-data2 = data.copy()
-print(data.count('murali'))
-data.extend([230,8541,'raju',99,'tarun'])
-
-print(data2)
-print(data)
+d={"Delhi":5, "Jammu":9, "Gujarath":8,  "Andhra":1, "Kerala":3}
+c=sorted(d.items())
+print(c)
+t = {x:y for x, y in c}
+print(t.values())
+print(t.keys())
+print(t)
