13971dbf50868ffde20a384e50225688c3f7bcef
murali <murali9295@gmail.com>
2022-09-28 13:01:09 +0530

    data programme

---
+++ data.py
@@ -0,0 +1,8 @@
+data = [2,3,'reddy',891,'murali',2,'MURALI']
+data.append(23)
+data2 = data.copy()
+print(data.count('murali'))
+data.extend([230,8541,'raju',99,'tarun'])
+
+print(data2)
+print(data)
